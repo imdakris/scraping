7c1a2d7208ee5e19d211d231b5ff01c64b3d92d4
Ruslan Dovhanych <imdakris@gmail.com>
2023-04-26 23:15:39 +0300

    add headers

--- o_O.py
+++ o_O.py
@@ -1,10 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
+from time import sleep
 
-for count in range(1,8):
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Host": "httpbin.org",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+    "X-Amzn-Trace-Id": "Root=1-6448ff31-4d1cbae5563ca825330bbc4c"
+}
+
+for count in range(1, 8):
+    sleep(3)
     url = f'https://scrapingclub.com/exercise/list_basic/?page={count}'
 
-    response = requests.get(url)
+    response = requests.get(url, headers=headers)
 
     soup = BeautifulSoup(response.text, 'lxml')  # html.parser
 
